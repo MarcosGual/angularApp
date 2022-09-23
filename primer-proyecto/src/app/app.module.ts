fb910f4f0ae0852e5c21e849cb476263016a8633
MarcosGual <marcos.gual@outlook.com>
2022-09-23 15:33:10 -0300

    setup proyecto articulos

--- primer-proyecto/src/app/app.module.ts
+++ primer-proyecto/src/app/app.module.ts
@@ -2,21 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IfElseComponent } from './if-else/if-else.component';
-import { CicloVidaComponent } from './ciclo-vida/ciclo-vida.component';
-import { FormularioPlantillaComponent } from './formulario-plantilla/formulario-plantilla.component';
 import { FormsModule } from '@angular/forms';
-import { ListadoPersonasComponent } from './listado-personas/listado-personas.component';
-import { AltaPersonaComponent } from './personas/alta-persona/alta-persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IfElseComponent,
-    CicloVidaComponent,
-    FormularioPlantillaComponent,
-    ListadoPersonasComponent,
-    AltaPersonaComponent
   ],
   imports: [
     BrowserModule,
